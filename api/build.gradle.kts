0c86ea33ff20dc693d26eb8a69a0e9dbe5285882
不要糖醋放椒盐 <moriafly@163.com>
2025-05-27 08:31:35 +0800

    更新项目配置

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,15 +1,22 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+        jvmTarget = JvmTarget.JVM_21
     }
+}
+
+dependencies {
+    implementation(libs.pf4j)
+    testImplementation(libs.junit)
 }
