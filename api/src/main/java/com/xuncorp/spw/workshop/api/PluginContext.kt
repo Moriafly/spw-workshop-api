de59ce94e3c41071e3e2fafe76a70202cf792ddb
GaodaGG <gaodagg50@gmail.com>
2025-09-09 22:09:00 +0800

    Add SpwPlugin and PluginContext classes for plugin management

---
+++ api/src/main/java/com/xuncorp/spw/workshop/api/PluginContext.kt
@@ -0,0 +1,13 @@
+package com.xuncorp.spw.workshop.api
+
+/**
+ * 插件上下文信息
+ *
+ * 提供插件所需的基本信息，如 ID、版本和路径
+ */
+data class PluginContext(
+    val pluginId: String,
+    val pluginVersion: String,
+    val pluginPath: String,
+    val spwVersion: String
+)
---
+++ api/src/main/java/com/xuncorp/spw/workshop/api/SpwPlugin.kt
@@ -0,0 +1,7 @@
+package com.xuncorp.spw.workshop.api
+
+import org.pf4j.Plugin
+
+open class SpwPlugin(
+    val pluginContext: PluginContext
+) : Plugin()
